459119db1ffdf58992a0c719eee5123db0f73a82
joycie416 <joycie416@gmail.com>
2025-03-07 17:09:16 +0900

    style: Data section 반응형 디자인 한단계 추가

--- src/components/main/data/DataContent.tsx
+++ src/components/main/data/DataContent.tsx
@@ -45,7 +45,7 @@ const DataContent = () => {
           </div>
         </div>
         <Logos />
-        <div className="grid grid-rows-[repeat(3,_1fr)] gap-5 mt-[20px] md:mt-[113px] md:grid-cols-[repeat(3,_1fr)] md:grid-rows-1 items-center">
+        <div className="grid grid-rows-[repeat(3,_1fr)] gap-5 mt-[20px] md:mt-[113px] md:grid-cols-[repeat(3,_1fr)] md:grid-rows-1  lg:min-h-[365px]">
           {["Search", "Meeting", "Deal"].map((title) => (
             <Card title={title} key={title} />
           ))}
