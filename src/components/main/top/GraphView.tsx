20dbec23ed52788525147f79d8b7aa10053fc6a5
joycie416 <joycie416@gmail.com>
2025-03-17 18:10:46 +0900

    fix: 모바일에서 A2A 그래프로 보이는 것 버블 그래프로 수정

--- src/components/main/top/GraphView.tsx
+++ src/components/main/top/GraphView.tsx
@@ -3,16 +3,19 @@
 import { useGraphContext } from "@/store/GraphContext";
 import AToAGraph from "./AToAGraph";
 import BubbleGraph from "./BubbleGraph";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const GraphView = () => {
   const { newNode } = useGraphContext((store) => store);
+  const isMobile = useIsMobile();
 
   console.log(newNode);
 
   return (
     <div className="bg-black ">
       {!newNode && <BubbleGraph />}
-      {newNode && <AToAGraph />}
+      {newNode && !isMobile && <AToAGraph />}
+      {newNode && isMobile && <BubbleGraph />}
     </div>
   );
 };
